1b6005730e3ec3cd75774a428bd4a51695d1f57e
Guadalupe-Alvarado <dai.22121994@gmail.com>
2024-04-24 19:56:10 -0300

    Algo de js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let email = document.querySelector("#email")
+let nombreUser = document.querySelector("#neme")
+let datos = document.querySelector("#datos")
+let btn = document.querySelector('#boton')
+
+
+document.querySelector("#boton").addEventListener('click', () => {
+
+    datos.textContent = email.value
+    datos.textContent = email.value
+
+})
+
+
+console.log(email.value)
